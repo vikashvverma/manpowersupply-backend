2c34386016aab1f9bdda6f1676f26a1aef373166
Vikash Kumar Verma <VKV@corp.castsoftware.com>
2017-12-11 12:41:54 +0530

    Setup DB migrations

---
+++ db/migrations/1512967961_setup-schema-and-table.up.sql
@@ -0,0 +1,49 @@
+CREATE SCHEMA manpower;
+
+CREATE TABLE manpower.party (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	address VARCHAR(100) NOT NULL,
+	phone varchar(15) NOT NULL,
+	mobile varchar(15) NOT NULL,
+	email varchar(20) NOT NULL
+);
+
+
+
+CREATE TABLE manpower.query (
+	id SERIAL,
+	queryer_id INT NOT NULL,
+	query varchar(150) NOT NULL,
+	query_date timestamptz  NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE manpower.job_type (
+	id SERIAL,
+	type_id INT NOT NULL UNIQUE,
+	industry varchar(10) NOT NULL UNIQUE,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE manpower.job (
+	id SERIAL,
+	job_id INT NOT NULL UNIQUE,
+	title varchar(10) NOT NULL,
+	industry varchar(10) NOT NULL,
+	location varchar(10) NOT NULL,
+	date_created timestamptz NOT NULL,
+	date_updated timestamptz NOT NULL,
+	available BOOLEAN NOT NULL,
+	type_id INT NOT NULL,
+	PRIMARY KEY (id)
+);
+
+
+
+ALTER TABLE manpower.query ADD CONSTRAINT query_fk0 FOREIGN KEY (queryer_id) REFERENCES manpower.party(id);
+
+ALTER TABLE manpower.job ADD CONSTRAINT job_fk0 FOREIGN KEY (industry) REFERENCES manpower.job_type(industry);
+
+ALTER TABLE manpower.job ADD CONSTRAINT job_fk1 FOREIGN KEY (type_id) REFERENCES manpower.job_type(type_id);
+
