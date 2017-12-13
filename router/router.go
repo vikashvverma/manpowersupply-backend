b961b3ceab8db65ee171eecce883275415a36be5
Vikash Kumar Verma <VKV@corp.castsoftware.com>
2017-12-13 19:55:43 +0530

    Save a party

--- router/router.go
+++ router/router.go
@@ -17,7 +17,9 @@ const (
 func Router(c *config.Config, f *factory.Factory) *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/healthcheck", healthcheck.Self).Methods(GET)
-	router.HandleFunc("/api/manpower/party", handlers.FindAllParty(f.Fetcher(), f.Logger())).Methods(GET)
+	router.HandleFunc("/api/manpower/party", handlers.SaveParty(f.Fetcher(), f.Logger())).Methods(POST)
+	router.HandleFunc("/api/manpower/party", handlers.FindParty(f.Fetcher(), f.Logger())).Methods(GET)
+	router.HandleFunc("/api/manpower/party/{page}", handlers.FindParty(f.Fetcher(), f.Logger())).Methods(GET)
 	//router.HandleFunc("/api/techscan/{lang}", handler.Language(f.Fetcher(), f.Logger())).Methods(GET)
 	//router.HandleFunc("/api/techscan/{lang}/{page}", handler.Language(f.Fetcher(), f.Logger())).Methods(GET)
 	//router.HandleFunc("/api/owner/{repoID}", handler.Owner(f.Fetcher(), f.Logger())).Methods(GET)
