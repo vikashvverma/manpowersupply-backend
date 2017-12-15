63466b4f92cbb2afd800b391aa3f22766531166c
Vikash Kumar Verma <VKV@corp.castsoftware.com>
2017-12-15 10:19:45 +0530

    Add route to upsert a job

--- router/router.go
+++ router/router.go
@@ -17,13 +17,13 @@ const (
 func Router(c *config.Config, f *factory.Factory) *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/healthcheck", healthcheck.Self).Methods(GET)
+	router.HandleFunc("/api/manpower/job", handlers.Upsert(f.JobFetcher(), f.Logger())).Methods(POST)
+	router.HandleFunc("/api/manpower/job", handlers.FindJob(f.JobFetcher(), f.Logger())).Methods(GET)
+	router.HandleFunc("/api/manpower/job", handlers.FindJob(f.JobFetcher(), f.Logger())).Queries("page", "{page}", "limit", "{limit}").Methods(GET)
+	router.HandleFunc("/api/manpower/job/{id}", handlers.FindJob(f.JobFetcher(), f.Logger())).Methods(GET)
 	router.HandleFunc("/api/manpower/party", handlers.SaveParty(f.PartyFetcher(), f.Logger())).Methods(POST)
 	router.HandleFunc("/api/manpower/party", handlers.FindParty(f.PartyFetcher(), f.Logger())).Methods(GET)
 	router.HandleFunc("/api/manpower/party", handlers.FindParty(f.PartyFetcher(), f.Logger())).Queries("page", "{page}").Methods(GET)
 	router.HandleFunc("/api/manpower/party/{id}", handlers.FindParty(f.PartyFetcher(), f.Logger())).Methods(GET)
-	//router.HandleFunc("/api/manpower/party", handlers.SaveParty(f.PartyFetcher(), f.Logger())).Methods(POST)
-	router.HandleFunc("/api/manpower/job", handlers.FindJob(f.JobFetcher(), f.Logger())).Methods(GET)
-	router.HandleFunc("/api/manpower/job", handlers.FindJob(f.JobFetcher(), f.Logger())).Queries("page", "{page}", "limit", "{limit}").Methods(GET)
-	router.HandleFunc("/api/manpower/job/{id}", handlers.FindJob(f.JobFetcher(), f.Logger())).Methods(GET)
 	return router
 }
